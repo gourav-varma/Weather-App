bd01697f8f8832c1fd1de60c5c41a2ee6e9c7bb8
gourav-varma <gouravswork@gmail.com>
2020-07-08 17:39:39 +0530

    0.1

---
+++ app/src/main/java/com/example/weatherapp/ui/settings/SettingsFragment.kt
@@ -0,0 +1,12 @@
+package com.example.weatherapp.ui.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : Fragment(){
+//    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+//        TODO("Not yet implemented")
+//    }
+
+}
